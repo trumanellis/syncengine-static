9ca4d0b9e98dbf677e9a283ed335c4808db7d139
Truman Ellis <truman.e.ellis@gmail.com>
2025-10-09 06:58:30 +0100

    feat: Increase logo size to 2.5x bigger (160x160px)

--- src/scripts/sacred-theme.js
+++ src/scripts/sacred-theme.js
@@ -21,7 +21,7 @@ function createNavigation() {
 
         navElement.innerHTML = `
             <a href="index.html" class="logo" aria-label="Synchronicity Engine Homepage">
-                <img src="media/Lila Gold.png" alt="Synchronicity Engine - Lila Gold Logo" class="logo-image" width="64" height="64">
+                <img src="media/Lila Gold.png" alt="Synchronicity Engine - Lila Gold Logo" class="logo-image" width="160" height="160">
                 <span class="logo-text">Synchronicity<br>Engine</span>
             </a>
             <ul class="nav-links" role="menubar">
